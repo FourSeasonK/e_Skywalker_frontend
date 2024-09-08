f202fe3880c1bf1c37199ca30118fe1043f7adab
FourSeasonK <fourseasonk77@gmail.com>
2024-09-07 20:52:29 -0700

    no.11 commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://localhost:8080/api",
-    baseURL: "https://eskywalkerbackend-production.up.railway.app/api",
+    baseURL: "http://localhost:8080/api",
+    // baseURL: "https://eskywalkerbackend-production.up.railway.app/api",
     headers: {
         "Content-type": "application/json"
     }    
